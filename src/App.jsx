0c6adb0ec0f497faa72090aaa2942c0beba6a23d
이수민 <soooper1137@etoos.com>
2024-11-20 16:54:28 +0900

    ant design 테마

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,28 @@ import Header from "./components/Header";
 import Home from "./pages/Hone";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import { ConfigProvider } from "antd";
 
 const App = () => {
   return (
-    <div>
+    // [ant design theme] 
+    // https://ant.design/docs/react/customize-theme
+    // https://ant.design/docs/react/customize-theme
+    <ConfigProvider 
+      theme={({
+        token: {
+          colorPrimary: '#ccc',
+          colorBgContainer: '#e0e0e0',
+        },
+      })}
+    >
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </div>
+    </ConfigProvider>
   );
 };
 
