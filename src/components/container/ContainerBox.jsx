03cd25da5c35f496ab2f3ec3b76cda0a2066eee2
이수민 <soooper1137@etoos.com>
2024-11-20 17:22:40 +0900

    ContainerBox 컴포넌트

--- src/components/container/ContainerBox.jsx
+++ src/components/container/ContainerBox.jsx
@@ -5,6 +5,19 @@ const ContainerBox = ({ className, children, style }) => {
   return (
     <section className={className} style={style}>
       {children}
+      <div className="inner">
+        <article className="img-box">
+          <figure>
+            <img src="" alt="" />
+            <figcaption></figcaption>
+          </figure>
+        </article>
+        <div className="txt-box">
+          <h2 className="tit"></h2>
+          <h3 className="tit _sub"></h3>
+          <p className="txt"></p>
+        </div>
+      </div>
     </section>
   );
 };
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,17 @@
+import ContainerBox from "../components/container/ContainerBox";
+
 const Menu = () => {
     return (
       <div>
-        메뉴추천 페이지
+        <ContainerBox>
+          
+        </ContainerBox>
+        {/* 메뉴추천 페이지
+
+        인기 메뉴(뜨는)
+
+        점메추 / 저메추 */}
+
       </div>
     );
   };
