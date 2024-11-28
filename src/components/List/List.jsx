966f6e5be7dbafdd3ef9efb5e75f89262d0389d7
이수민 <soooper1137@etoos.com>
2024-11-28 17:43:08 +0900

    리스트컴포넌트 추가

---
+++ src/components/List/List.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Fragment } from "react";
+
+const ListCp = ({ titles, items, children }) => {
+  return (
+    <ul className="list">
+      {titles.map((item, i) => (
+        <Fragment key={i}>
+          <li>
+            <article className={item.titleClass}>
+              {item.text}
+              <figure>
+                <img src={item.imageSrc || ""} alt={item.imageAlt || "Image"} />
+                <figcaption>{item.title}</figcaption>
+              </figure>
+            </article>
+            <div className="txt-box">
+              <h2 className="tit">{item.title}</h2>
+              {item.title2 && <h3 className="tit _sub">{item.title2}</h3>}
+              {item.txt2 && <p className="txt">{item.txt2}</p>}
+              {/* children에서 해당 slot을 찾거나 기본 값을 렌더링 */}
+              {children?.[`item.${item.value}`]
+                ? React.cloneElement(children[`item.${item.value}`], { items })
+                : items[item.value]}
+            </div>
+          </li>          
+        </Fragment>
+      ))}
+    </ul>
+  );
+};
+
+ListCp.propTypes = {
+  titles: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      titleClass: PropTypes.string,
+      itemClass: PropTypes.string,
+      value: PropTypes.string,
+      imageSrc: PropTypes.string,
+      imageAlt: PropTypes.string,
+    })
+  ).isRequired,
+  items: PropTypes.object.isRequired,
+};
+
+
+export default ListCp;
--- src/pages/Hone.jsx
+++ src/pages/Hone.jsx
@@ -5,6 +5,51 @@ import Button from "../components/common/Button/Button";
 import { antdTheme } from "../styles/theme";
 import Modal from "../components/Layouts/Modal";
 import FlexBox from "../components/Layouts/Flexbox";
+import ListCp from "../components/List/List";
+
+const titles = [
+  {
+    text: "글1",
+    title: "제목1",
+    titleClass: "title-class-1",
+    itemClass: "item-class-1",
+    value: "key1",
+    imageSrc: "image1.jpg",
+    imageAlt: "Image 1",
+    title2: "Sub Title 1",
+    txt2: "Additional text for item 1",
+  },
+  {
+    text: "글2",
+    title: "제목2",
+    titleClass: "title-class-2",
+    itemClass: "item-class-2",
+    value: "key2",
+    imageSrc: "image2.jpg",
+    imageAlt: "Image 2",
+    txt2: "Additional text for item 2",
+  },
+  {
+    text: "글3",
+    title: "제목3",
+    titleClass: "title-class-2",
+    itemClass: "item-class-2",
+    value: "key2",
+    imageSrc: "image2.jpg",
+    imageAlt: "Image 2",
+    txt2: "Additional text for item 2",
+  },
+];
+
+const items = {
+  key1: "Item 1 Content",
+  key2: "Item 2 Content",
+}
+
+// const items = [
+//   { key: "key1", content: "Item 1 Content" },
+//   { key: "key2", content: "Item 2 Content" },
+// ];
 
 const Home = () => {
   const [isModalVisible, setModalVisible] = useState(false);
@@ -18,8 +63,17 @@ const Home = () => {
       <Wrapper className="wrapper-1400"></Wrapper>
 
       <div>
+        <ListCp titles={titles} items={items}>
+          {{ 
+            "item.key1": <div>Custom Content for Key 1</div>,
+            "item.key2": <div>Custom Content for Key 2</div>,
+          }}
+        </ListCp>
+
         <DatePicker />
+        
         <Button size="md" theme="primary_1" label="test"></Button>
+        
         <>          
             <Button onClick={() => setModalVisible(true)} size="md" theme="primary_1" label="showModal"></Button>
             <Modal 
