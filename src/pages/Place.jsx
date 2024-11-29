1df99dcafdf982bf946eac7fe59c44f70473b6e0
이수민 <soooper1137@etoos.com>
2024-11-29 14:44:02 +0900

    gsap 추가

--- src/pages/Place.jsx
+++ src/pages/Place.jsx
@@ -1,5 +1,36 @@
-const Place = () => {
-  return <div>장소추천 페이지</div>;
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(useGSAP);
+
+const Place = () => {  
+  const container = useRef();
+
+    useGSAP(
+        () => {
+            // gsap code here...
+            gsap.to('.box', { rotation: 180 }); // <-- automatically reverted
+        },
+        { scope: container }
+    ); 
+
+  return (
+
+
+    <div className="place">
+      
+      <section className="wrapper-1400">
+        {/* <p className="txt txt1">txt1</p>
+        <p className="txt txt2">txt2</p> */}
+        
+      </section>
+
+      <div ref={container} className="app">
+        <div className="box">Hello</div>
+      </div>
+    </div>
+  );
 };
 
 export default Place;
