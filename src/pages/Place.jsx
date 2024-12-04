d824e21954412f73dd9655e4037a0926c0a5492a
이수민 <soooper1137@etoos.com>
2024-12-04 17:09:14 +0900

    swiper data

--- src/pages/Place.jsx
+++ src/pages/Place.jsx
@@ -1,24 +1,24 @@
 import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap/dist/gsap";
-import { useGSAP } from "@gsap/react/dist";    
+import { useGSAP } from "@gsap/react/dist";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
-import { Carousel } from 'antd';
+import { Carousel } from "antd";
 const contentStyle = {
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
+  height: "160px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
 };
 
-gsap.registerPlugin(useGSAP,ScrollTrigger);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Place = () => {
   const container = useRef();
@@ -27,7 +27,7 @@ const Place = () => {
   const tl2 = useRef(null);
 
   useGSAP(
-    () => {    
+    () => {
       gsap.to(".cont_1", { rotation: 360 });
     },
     { scope: container }
@@ -35,51 +35,77 @@ const Place = () => {
 
   useEffect(() => {
     const visualMotion = () => {
-      gsap.set (
-        ".obj1, obj2, obj3, obj4, obj5",
-        {delay: 0, opacity: 0, duration: 0, ease: "none"}
-      );
+      gsap.set(".obj1, obj2, obj3, obj4, obj5", {
+        delay: 0,
+        opacity: 0,
+        duration: 0,
+        ease: "none",
+      });
 
-      tl1.current = gsap.timeline({ onComplete: () => setIsActiveMotion(true) })
-          .to(".obj0", { delay: 0, opacity: 1, duration: 0.3 })
-          .to(".obj1", { delay: -0.1, opacity: 1, duration: 0.3 })
-          .to(".obj2", { delay: -0.1, opacity: 1, duration: 0.3 })
-          .to(".obj3", { delay: -0.2, opacity: 1, duration: 0.3 })
-          .to(".obj4", { delay: -0.3, opacity: 1, duration: 0.3 })          
-      };
+      tl1.current = gsap
+        .timeline({ onComplete: () => setIsActiveMotion(true) })
+        .to(".obj0", { delay: 0, opacity: 1, duration: 0.3 })
+        .to(".obj1", { delay: -0.1, opacity: 1, duration: 0.3 })
+        .to(".obj2", { delay: -0.1, opacity: 1, duration: 0.3 })
+        .to(".obj3", { delay: -0.2, opacity: 1, duration: 0.3 })
+        .to(".obj4", { delay: -0.3, opacity: 1, duration: 0.3 });
+    };
 
-      const sectionScrollMotion = () => {
-        tl2.current = gsap.timeline()
-          .to(".img1", { opacity: 1, y: 0, duration: 0.5 })
-          .to(".img1", { opacity: 1, duration: 1 })
-          .to(".img1", { opacity: 0, y: -50, duration: 0.1 })
-          .to(".img2", { opacity: 1, y: 0, duration: 0.5 })
-          .to(".img2", { opacity: 1, duration: 1 })
-          .to(".img2", { opacity: 0, y: -50, duration: 0.1 });
+    const sectionScrollMotion = () => {
+      tl2.current = gsap
+        .timeline()
+        .to(".img1", { opacity: 1, y: 0, duration: 0.5 })
+        .to(".img1", { opacity: 1, duration: 1 })
+        .to(".img1", { opacity: 0, y: -50, duration: 0.1 })
+        .to(".img2", { opacity: 1, y: 0, duration: 0.5 })
+        .to(".img2", { opacity: 1, duration: 1 })
+        .to(".img2", { opacity: 0, y: -50, duration: 0.1 });
 
-        ScrollTrigger.create({
-          trigger: ".visual",
-          start: "top",
-          end: "bottom+=50%",
-          pin: true,
-          pinSpacing: true,
-          animation: tl2.current,
-          scrub: 0.5,
-        });
-      };
+      ScrollTrigger.create({
+        trigger: ".visual",
+        start: "top",
+        end: "bottom+=50%",
+        pin: true,
+        pinSpacing: true,
+        animation: tl2.current,
+        scrub: 0.5,
+      });
+    };
 
-      visualMotion();
-      sectionScrollMotion();
+    visualMotion();
+    sectionScrollMotion();
 
-      return () => {
-        ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-      };
-    }, []);
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
+
+  const ani1Slides = [
+    { id: 1, branchName: "Branch 1", name: "Name 1", univ: "University 1" },
+    { id: 2, branchName: "Branch 2", name: "Name 2", univ: "University 2" },
+    { id: 3, branchName: "Branch 3", name: "Name 3", univ: "University 3" },
+  ];
+
+  const ani2Slides = [
+    { id: 4, branchName: "Branch 4", name: "Name 4", univ: "University 4" },
+    { id: 5, branchName: "Branch 5", name: "Name 5", univ: "University 5" },
+    { id: 6, branchName: "Branch 6", name: "Name 6", univ: "University 6" },
+  ];
+
+  const commonSwiperOption = {
+    loop: true,
+    autoplay: { delay: 0, disableOnInteraction: false },
+    speed: 5000,
+    sliderPerView: "auto",
+    spaceBetween: 10,
+    allowTouchMove: false,
+    observer: true,
+    observeParent: true,
+  };
 
   return (
-    <div className="place">     
+    <div className="place">
       <div ref={container}>
-
         <section className={`visual ${isActiveMotion ? "is_active" : ""}`}>
           <div className="wrapper-1400"></div>
           <span className="obj_box">
@@ -87,7 +113,7 @@ const Place = () => {
             <em className="obj obj1"></em>
             <em className="obj obj2"></em>
             <em className="obj obj3"></em>
-            <em className="obj obj4"></em>            
+            <em className="obj obj4"></em>
           </span>
           <span className="img_box">
             <img className="img img1" src="" alt="" />
@@ -104,8 +130,8 @@ const Place = () => {
 
         <section className="cont_2 place_cont">
           <div className="wrapper-1400">
-          {/* 무한 좌우롤링 각 역방향 */}
-            <Swiper        
+            {/* 무한 좌우롤링 각 역방향 */}
+            <Swiper
               modules={[Navigation, Pagination, Scrollbar, A11y]}
               spaceBetween={50}
               slidesPerView={3}
@@ -113,16 +139,14 @@ const Place = () => {
               pagination={{ clickable: true }}
               scrollbar={{ draggable: true }}
               onSwiper={(swiper) => console.log(swiper)}
-              onSlideChange={() => console.log('slide change')}
+              onSlideChange={() => console.log("slide change")}
               autoplay={true}
             >
               <SwiperSlide>Slide 1</SwiperSlide>
-              <SwiperSlide>Slide 2</SwiperSlide>
-              <SwiperSlide>Slide 3</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 1</SwiperSlide>
             </Swiper>
 
-            <Swiper        
+            <Swiper
               modules={[Navigation, Pagination, Scrollbar, A11y]}
               spaceBetween={50}
               slidesPerView={3}
@@ -130,7 +154,7 @@ const Place = () => {
               pagination={{ clickable: true }}
               scrollbar={{ draggable: true }}
               onSwiper={(swiper) => console.log(swiper)}
-              onSlideChange={() => console.log('slide change')}
+              onSlideChange={() => console.log("slide change")}
               autoplay={true}
             >
               <SwiperSlide>Slide 1</SwiperSlide>
@@ -143,7 +167,12 @@ const Place = () => {
 
         <section className="cont_3 place_cont">
           <div className="wrapper-1400">
-            <Carousel effect="scrollx" easing="linear" infinite="true" draggable="true">
+            <Carousel
+              effect="scrollx"
+              easing="linear"
+              infinite="true"
+              draggable="true"
+            >
               <div>
                 <h3 style={contentStyle}>1</h3>
               </div>
@@ -161,7 +190,35 @@ const Place = () => {
         </section>
 
         <section className="cont_4 place_cont">
-          <div className="wrapper-1400"></div>
+          <div className="wrapper-1400">
+            <div className="swiper-container">
+              <Swiper {...commonSwiperOption}>
+                {ani1Slides.map((slide) => {
+                  <SwiperSlide key={slide.id}>
+                    <div className="slide-content">
+                      <p>{slide.branchName}</p>
+                      <p>{slide.name}</p>
+                      <p>{slide.univ}</p>
+                    </div>
+                  </SwiperSlide>;
+                })}
+              </Swiper>
+            </div>
+
+            <div className="swiper-container">
+              <Swiper {...commonSwiperOption}>
+                {ani2Slides.map((slide) => (
+                  <SwiperSlide key={slide.id}>
+                    <div className="slide-content">
+                      <p>{slide.branchName}</p>
+                      <p>{slide.name}</p>
+                      <p>{slide.univ}</p>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
         </section>
 
         <section className="cont_5 place_cont">
