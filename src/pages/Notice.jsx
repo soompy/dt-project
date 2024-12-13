bca9465ac2ae1fb537c093adb6e1e2502523bb0b
이수민 <soooper1137@etoos.com>
2024-12-13 17:26:36 +0900

    공지사항 등록/수정 기능 적용 예정

--- src/pages/Notice.jsx
+++ src/pages/Notice.jsx
@@ -4,6 +4,8 @@ const Notice = () => {
     return (
         <div>
             <BoardCp></BoardCp>
+            공지사항 작성(상세/수정) 페이지(추가완,기능구현 필요) 공지사항 확인
+            페이지(게시글 클릭 시 공지글 노출)
         </div>
     );
 };
