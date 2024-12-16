6f897738a73fc5923f751fad94a2f4ccb527e866
이수민 <soooper1137@etoos.com>
2024-12-16 17:13:58 +0900

    input box maxlength 제거

--- src/components/common/Input/Input.jsx
+++ src/components/common/Input/Input.jsx
@@ -1,23 +1,22 @@
 import PropTypes from "prop-types";
 
 const InputArea = ({ placeholderValue, type }) => {
-  return (
-    <div className="input_area">
-      <div className="input_field">
-        <input
-          className="search_field"
-          maxLength="4"
-          placeholder={placeholderValue}
-          type={type}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="input_area">
+            <div className="input_field">
+                <input
+                    className="input_box"
+                    placeholder={placeholderValue}
+                    type={type}
+                />
+            </div>
+        </div>
+    );
 };
 
 InputArea.propTypes = {
-  placeholderValue: PropTypes.string,
-  type: PropTypes.string,
+    placeholderValue: PropTypes.string,
+    type: PropTypes.string,
 };
 
 export default InputArea;
