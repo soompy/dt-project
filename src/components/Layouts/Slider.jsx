3c2f7638ddb05969d4a87dcb276af37c527b0b78
이수민 <soooper1137@etoos.com>
2024-12-16 15:41:51 +0900

    swiper slide 역방향 옵션 추가

--- src/components/Layouts/Slider.jsx
+++ src/components/Layouts/Slider.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import {
     Autoplay,
     Navigation,
@@ -12,7 +12,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-const SliderCp = ({ bannerList }) => {
+const SliderCp = ({ bannerList, isReverse = false }) => {
     const [activeIdx, setActiveIdx] = useState(0);
 
     return (
@@ -23,7 +23,8 @@ const SliderCp = ({ bannerList }) => {
                 slidesPerView={1}
                 onSlideChange={(e) => setActiveIdx(e.activeIdx)}
                 autoplay={{
-                    delay: 4500,
+                    delay: 800,
+                    reverseDirection: isReverse,
                     disableOnInteraction: false,
                 }}
                 speed={500}
@@ -38,7 +39,9 @@ const SliderCp = ({ bannerList }) => {
                             className={`desc-box ${
                                 banner.id === activeIdx ? "active" : ""
                             }`}
-                        ></span>
+                        >
+                            {banner.txt}
+                        </span>
                     </SwiperSlide>
                 ))}
             </Swiper>
