96242d7b0e439a3a097dfd2a38b3061cb43cabd6
이수민 <soooper1137@etoos.com>
2024-12-19 09:18:40 +0900

    vite preprocessorOptions 옵션 추가

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ], 
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    assetsInlineLimit: 8192,
-  },
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern-compiler",
+            },
+        },
+    },
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+        assetsInlineLimit: 8192,
+    },
 });
