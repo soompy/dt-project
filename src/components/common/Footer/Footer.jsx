820c3767f6bf3cf1f6a1793cc6281e04f54619a6
이수민 <sooper1137@gmail.com>
2025-01-02 00:01:26 +0900

    푸터

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,10 +1,38 @@
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="logo">
-                <h1>로고</h1>
+            <div className="footer__content">
+                <div className="footer__logo">
+                    <h1>로고</h1>
+                </div>
+                <div className="footer__nav">
+                    <ul>
+                        <li>
+                            <a href="/about">회사 소개</a>
+                        </li>
+                        <li>
+                            <a href="/services">서비스</a>
+                        </li>
+                        <li>
+                            <a href="/contact">문의하기</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="footer__social">
+                    <a href="" target="_blank" rel="noopener noreferrer">
+                        Facebook
+                    </a>
+                    <a href="" target="_blank" rel="noopener noreferrer">
+                        Twitter
+                    </a>
+                    <a href="" target="_blank" rel="noopener noreferrer">
+                        Instagram
+                    </a>
+                </div>
             </div>
-            <p>푸푸푸터</p>
+            <div className="footer__bottom">
+                <p>&copy; {new Date().getFullYear()} copyright sm. All rights reserved.</p>
+            </div>
         </footer>
     );
 };
