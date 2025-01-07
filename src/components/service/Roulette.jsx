9c26f1ca11dbc268e32614e9d115120c6a77f8b4
이수민 <soooper1137@etoos.com>
2025-01-07 17:09:47 +0900

    룰렛 재활용성 추가

--- src/components/service/Roulette.jsx
+++ src/components/service/Roulette.jsx
@@ -1,31 +1,9 @@
 import { useState } from "react";
 import Wrapper from "../Layouts/Wrapper";
 import { Wheel } from "react-custom-roulette";
+import PropTypes from "prop-types";
 
-const data = [
-    {
-        option: "Apple Vision Pro",
-        style: { backgroundColor: "gray", textColor: "white" },
-        percentage: 3,
-    },
-    {
-        option: "LG TV",
-        style: { backgroundColor: "red", textColor: "white" },
-        percentage: 7,
-    },
-    {
-        option: "SAMSUNG 에어컨",
-        style: { backgroundColor: "blue", textColor: "white" },
-        percentage: 10,
-    },
-    {
-        option: "꽝",
-        style: { backgroundColor: "white", textColor: "red" },
-        percentage: 80,
-    },
-];
-
-const RouletteGame = () => {
+const RouletteGame = ({ data }) => {
     const [mustSpin, setMustSpin] = useState(false);
     const [prizeNumber, setPrizeNumber] = useState(0);
 
@@ -34,11 +12,9 @@ const RouletteGame = () => {
             const pivot = Math.floor(Math.random() * 99 + 1);
             let stack = 0;
 
-            const percentage = data.map((row) => row.percentage);
             let newPrizeNumber = null;
-
-            percentage.some((row, idx) => {
-                stack += row;
+            data.some((row, idx) => {
+                stack += row.percentage;
                 if (pivot <= stack) {
                     newPrizeNumber = idx;
                     return true;
@@ -62,14 +38,18 @@ const RouletteGame = () => {
 
                 <div className="roulette">
                     <Wheel
-                        spinDuration={0.2} // 스핀 속도
-                        startingOptionIndex={Math.floor(
-                            Math.random() * data.length
-                        )} // 초기 위치 랜덤
-                        mustStartSpinning={mustSpin} // 회전 여부
-                        prizeNumber={prizeNumber} // 당첨 인덱스
-                        data={data} // 룰렛 데이터
-                        onStopSpinning={StopSpinning} // 회전 종료 시 동작
+                        spinDuration={0.2}
+                        startingOptionIndex={Math.floor(Math.random() * data.length)}
+                        mustStartSpinning={mustSpin}
+                        prizeNumber={prizeNumber}
+                        data={data.map((item) => ({
+                            ...item,
+                            style: {
+                                backgroundColor: item.backgroundColor,
+                                textColor: item.textColor,
+                            },
+                        }))}
+                        onStopSpinning={StopSpinning}
                     />
                     <button onClick={handleSpinClick}>SPIN</button>
                     <div>{prizeNumber}</div>
@@ -77,6 +57,11 @@ const RouletteGame = () => {
             </Wrapper>
         </>
     );
+};
+
+
+RouletteGame.propTypes = {
+    data: PropTypes.string,    
 };
 
 export default RouletteGame;
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import RouletteGame from "../components/service/Roulette";
 
 const Game = () => {
+    const rouletteData = [
+        {
+            option: "Apple Vision Pro",
+            backgroundColor: "gray",
+            textColor: "white",
+            percentage: 3,
+        },
+        {
+            option: "LG TV",
+            backgroundColor: "red",
+            textColor: "white",
+            percentage: 7,
+        },
+        {
+            option: "SAMSUNG 에어컨",
+            backgroundColor: "blue",
+            textColor: "white",
+            percentage: 10,
+        },
+        {
+            option: "꽝",
+            backgroundColor: "white",
+            textColor: "red",
+            percentage: 80,
+        },
+    ];
+    
     <>
-        <RouletteGame></RouletteGame>
-    </>
-}
+        <RouletteGame data={rouletteData} />;
+        {/* 벽돌깨기
+        뱀꼬리잡기 등 */}
+    </>;
+};
 
 export default Game;
